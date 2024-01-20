abd1b05e78085ce50bc05afb06a1713b2a7cee3f
jacopo-derrico <derricoj.design@gmail.com>
2024-01-20 16:10:23 +0100

    team array created

---
+++ main.js
@@ -0,0 +1,33 @@
+let teamHTML = document.getElementById('team');
+let team = [ 
+   {
+     name: "Wayne Barnett",
+     role: "Founder & Ceo",
+     picture: "",
+   },
+   {
+     name: "Angela Caroll",
+     role: "Chief Editor",
+     picture: "",
+   },
+   {
+     name: "Walter Gordon",
+     role: "Office Manager",
+     picture: "",
+   },
+   {
+     name: "Angela Lopez",
+     role: "Social Media Manager",
+     picture: "",
+   },
+   {
+     name: "Scott Estrada",
+     role: "Developer",
+     picture: "",
+   },
+   {
+     name: "Barbara Ramos",
+     role: "Graphic Designer",
+     picture: "",
+   },
+];
